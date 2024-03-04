ff432bc080f240d694bd6ef8b52e719d61a95b81
DenGian <mondelaers.ian@gmail.com>
2024-03-04 23:36:00 +0100

    did some formatting

--- Book.cs
+++ Book.cs
@@ -2,210 +2,210 @@
 {
     public class Book
     {
-    // Eigenschappen
-    private string title;
-    public string Title
-    {
-        get { return title; }
-        set
+        // Eigenschappen
+        private string title;
+        public string Title
         {
-            if (string.IsNullOrWhiteSpace(value))
+            get { return title; }
+            set
             {
-                throw new ArgumentException("Titel mag niet leeg zijn.");
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Titel mag niet leeg zijn.");
+                }
+                title = value;
             }
-            title = value;
         }
-    }
-    private string isbn;
-    public string ISBN
-    {
-        get { return isbn; }
-        set
+        private string isbn;
+        public string ISBN
         {
-            if(!ValidISBN(value))
+            get { return isbn; }
+            set
             {
-                throw new ArgumentException("Ongeldig ISBN");
+                if(!ValidISBN(value))
+                {
+                    throw new ArgumentException("Ongeldig ISBN");
+                }
+                isbn = value;
             }
-            isbn = value;
         }
-    }
-    private string author;
-    public string Author
-    {
-        get { return author; }
-        set
+        private string author;
+        public string Author
         {
-            author = value;
+            get { return author; }
+            set
+            {
+                author = value;
+            }
         }
-    }
-    private DateTime releaseDate;
-    public DateTime ReleaseDate
-    {
-        get { return releaseDate; }
-        set 
+        private DateTime releaseDate;
+        public DateTime ReleaseDate
         {
-            if(value > DateTime.Now)
+            get { return releaseDate; }
+            set 
             {
-                throw new ArgumentException("ReleaseDatum kan niet in de toekomst liggen.");
+                if(value > DateTime.Now)
+                {
+                    throw new ArgumentException("ReleaseDatum kan niet in de toekomst liggen.");
+                }
+                releaseDate = value;
             }
-            releaseDate = value;
         }
-    }
-    private int numberOfPages;
-    public int NumberOfPages
-    {
-        get { return numberOfPages; }
-        set 
-        { 
-            if(value <= 0)
+        private int numberOfPages;
+        public int NumberOfPages
+        {
+            get { return numberOfPages; }
+            set 
+            { 
+                if(value <= 0)
+                {
+                    throw new ArgumentException("Aantal pagina's moet een positief getal zijn.");
+                }
+                numberOfPages = value;
+            }
+        }
+        
+        private Genre genre;
+        public Genre BookGenre
+        {
+            get { return genre; }
+            set
             {
-                throw new ArgumentException("Aantal pagina's moet een positief getal zijn.");
+                genre = value;
             }
-            numberOfPages = value;
         }
-    }
+        private string publisher;
+        public string Publisher
+        {
+            get { return publisher; }
+            set
+            {
+                publisher = value;
+            }
+        }
     
-    private Genre genre;
-    public Genre BookGenre
-    {
-        get { return genre; }
-        set
+        private decimal price;
+        public decimal Price
         {
-            genre = value;
-        }
-    }
-    private string publisher;
-    public string Publisher
-    {
-        get { return publisher; }
-        set
-        {
-            publisher = value;
-        }
-    }
-
-    private decimal price;
-    public decimal Price
-    {
-        get { return price; }
-        set
-        {
-            if (value < 0)
+            get { return price; }
+            set
             {
-                throw new ArgumentException("Prijs kan niet negatief zijn.");
+                if (value < 0)
+                {
+                    throw new ArgumentException("Prijs kan niet negatief zijn.");
+                }
+                price = value;
             }
-            price = value;
         }
-    }
-    private Library library;
-    public Library Library
-    {
-        get { return library; }
-        set
+        private Library library;
+        public Library Library
         {
-            library = value;
+            get { return library; }
+            set
+            {
+                library = value;
+            }
         }
-    }
     
-    // methodes
-    public static bool ValidISBN(string isbn)
-    {
-        string fullIsbn = isbn.Replace("-", "");
-        if(fullIsbn.Length != 13)
+        // methodes
+        public static bool ValidISBN(string isbn)
         {
-            return false;
-        }
-        foreach(char c in fullIsbn)
-        {
-            if(!char.IsDigit(c))
+            string fullIsbn = isbn.Replace("-", "");
+            if(fullIsbn.Length != 13)
             {
                 return false;
             }
+            foreach(char c in fullIsbn)
+            {
+                if(!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
-    }
 
-    public void AllBookInfo()
-    {
-        Console.WriteLine($"Titel: {Title}");
-        Console.WriteLine($"ISBN: {ISBN}");
-        Console.WriteLine($"Auteur: {Author}");
-        Console.WriteLine($"Datum van uitgave: {ReleaseDate.ToString("dd-MM-yyyy")}");
-        Console.WriteLine($"Aantal pagina's: {NumberOfPages}");
-        Console.WriteLine($"Genre: {BookGenre}");
-        Console.WriteLine($"Uitgever: {Publisher}");
-        Console.WriteLine($"Prijs: {Price}");
-    }
-
-public static List<Book> DeserializeBooksFromCsv(string csvFilePath)
-{
-    List<Book> books = new List<Book>();
-
-    Console.WriteLine("Reading books from CSV file...");
-    string[] lines = File.ReadAllLines(csvFilePath);
-
-    Console.WriteLine("Parsing CSV data...");
-    foreach (string line in lines.Skip(1))
-    {
-        string[] data = line.Split(',');
-
-        if (data.Length != 8)
+        public void AllBookInfo()
         {
-            Console.WriteLine("Incomplete data in CSV file. Skipping line.");
-            continue;
+            Console.WriteLine($"Titel: {Title}");
+            Console.WriteLine($"ISBN: {ISBN}");
+            Console.WriteLine($"Auteur: {Author}");
+            Console.WriteLine($"Datum van uitgave: {ReleaseDate.ToString("dd-MM-yyyy")}");
+            Console.WriteLine($"Aantal pagina's: {NumberOfPages}");
+            Console.WriteLine($"Genre: {BookGenre}");
+            Console.WriteLine($"Uitgever: {Publisher}");
+            Console.WriteLine($"Prijs: {Price}");
         }
 
-        string title = data[0];
-        string isbn = data[1];
-        string author = data[2];
-        DateTime releaseDate = DateTime.Parse(data[3]);
-        int numberOfPages = int.Parse(data[4]);
-        Genre genre;
-        if (!Enum.TryParse(data[5], true, out genre))
+        public static List<Book> DeserializeBooksFromCsv(string csvFilePath)
         {
-            Console.WriteLine($"Invalid genre '{data[5]}'. Skipping book creation.");
-            continue;
+            List<Book> books = new List<Book>();
+
+            Console.WriteLine("Reading books from CSV file...");
+            string[] lines = File.ReadAllLines(csvFilePath);
+
+            Console.WriteLine("Parsing CSV data...");
+            foreach (string line in lines.Skip(1))
+            {
+                string[] data = line.Split(',');
+
+                if (data.Length != 8)
+                {
+                    Console.WriteLine("Incomplete data in CSV file. Skipping line.");
+                    continue;
+                }
+
+            string title = data[0];
+            string isbn = data[1];
+            string author = data[2];
+            DateTime releaseDate = DateTime.Parse(data[3]);
+            int numberOfPages = int.Parse(data[4]);
+            Genre genre;
+            if(!Enum.TryParse(data[5], true, out genre))
+            {
+                Console.WriteLine($"Invalid genre '{data[5]}'. Skipping book creation.");
+                continue;
+            }
+            string publisher = data[6];
+            decimal price = decimal.Parse(data[7]);
+
+            Book book = new Book(title, isbn, author, releaseDate, numberOfPages, genre, publisher, price);
+
+            books.Add(book);
+            }
+
+            Console.WriteLine($"Successfully deserialized {books.Count} books from CSV file.");
+            Console.WriteLine("");
+            return books;
         }
-        string publisher = data[6];
-        decimal price = decimal.Parse(data[7]);
 
-        Book book = new Book(title, isbn, author, releaseDate, numberOfPages, genre, publisher, price);
+        // Enum voor het genre van het boek
+        public enum Genre
+        {
+            Fiction,
+            NonFiction,
+            Romance,
+            Thriller,
+            ScienceFiction
+        }
 
-        books.Add(book);
-    }
-
-    Console.WriteLine($"Successfully deserialized {books.Count} books from CSV file.");
-    Console.WriteLine("");
-    return books;
-}
-
-    // Enum voor het genre van het boek
-    public enum Genre
-    {
-        Fiction,
-        NonFiction,
-        Romance,
-        Thriller,
-        ScienceFiction
-    }
-
-    //constructors
-    public Book(string title, string author, Library library)
-    {
-        this.Title = title;
-        this.Author = author;
-        this.Library = library;
-    }
-    public Book(string title, string isbn, string author, DateTime releaseDate, int numberOfPages, Genre genre, string publisher, decimal price)
-    {
-    this.Title = title;
-    this.ISBN = isbn;
-    this.Author = author;
-    this.ReleaseDate = releaseDate;
-    this.NumberOfPages = numberOfPages;
-    this.BookGenre = genre;
-    this.Publisher = publisher;
-    this.Price = price;
-    }       
+        //constructors
+        public Book(string title, string author, Library library)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Library = library;
+        }
+        public Book(string title, string isbn, string author, DateTime releaseDate, int numberOfPages, Genre genre, string publisher, decimal price)
+        {
+            this.Title = title;
+            this.ISBN = isbn;
+            this.Author = author;
+            this.ReleaseDate = releaseDate;
+            this.NumberOfPages = numberOfPages;
+            this.BookGenre = genre;
+            this.Publisher = publisher;
+            this.Price = price;
+        }       
     }   
 }
