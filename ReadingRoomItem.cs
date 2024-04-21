6c54198c94db84cedda9501c781a8c4ae8d9ce8b
DenGian <mondelaers.ian@gmail.com>
2024-04-21 13:18:17 +0200

    add abstract class for newspaper and magazine class

---
+++ ReadingRoomItem.cs
@@ -0,0 +1,26 @@
+﻿namespace bib_ian_mondelaers;
+
+internal abstract class ReadingRoomItem
+{
+    // Eigenschappen
+    private string title;
+    public string Title
+    {
+        get { return title; }
+    }
+    private string publisher;
+    public string Publisher
+    {
+        get { return publisher; }
+        set { publisher = value; }
+    }
+    public abstract string Identification { get; }
+    public abstract string Categorie { get; }
+
+    // Constructors
+    public ReadingRoomItem(string title, string publisher)
+    {
+        this.title = title;
+        this.publisher = publisher;
+    }
+}
