51a78866317ae99f539af367b2750dddd773be39
DenGian <mondelaers.ian@gmail.com>
2024-05-21 09:21:53 +0200

    made custom exception for titles

---
+++ InvalidTitleException.cs
@@ -0,0 +1,9 @@
+﻿namespace bib_ian_mondelaers;
+
+class InvalidTitleException: ApplicationException
+{
+    public override string ToString()
+    {
+        return "Titel mag niet leeg zijn.";
+    }
+}
