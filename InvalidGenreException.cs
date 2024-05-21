26a62ceaf9eeebe3ac29af3596e2d668565ffa79
DenGian <mondelaers.ian@gmail.com>
2024-05-21 09:20:02 +0200

    made custom exception for genre

---
+++ InvalidGenreException.cs
@@ -0,0 +1,9 @@
+﻿namespace bib_ian_mondelaers;
+
+class InvalidGenreException : ApplicationException
+{
+    public override string ToString()
+    {
+        return "Dit is een ongeldig genre.";
+    }
+}
