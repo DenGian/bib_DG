44235d0a8f5995e076f1279ceff56e3d81185a75
DenGian <mondelaers.ian@gmail.com>
2024-05-21 09:21:32 +0200

    made custom exception for release dates

---
+++ InvalidReleaseDateException.cs
@@ -0,0 +1,9 @@
+﻿namespace bib_ian_mondelaers;
+
+class InvalidReleaseDateException: ApplicationException
+{
+    public override string ToString()
+    {
+        return "De datum van uitgave kan niet in de toekomst liggen.";
+    }
+}
