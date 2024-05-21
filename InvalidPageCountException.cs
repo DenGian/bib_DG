76954364c045a0a6b0d27e336313c4606e438c8e
DenGian <mondelaers.ian@gmail.com>
2024-05-21 09:20:39 +0200

    made custom exception for page count

---
+++ InvalidPageCountException.cs
@@ -0,0 +1,9 @@
+﻿namespace bib_ian_mondelaers;
+
+class InvalidPageCountException: ApplicationException
+{
+    public override string ToString()
+    {
+        return "Aantal pagina's moet een positief getal zijn.";
+    }
+}
